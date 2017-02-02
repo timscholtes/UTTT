7c05e644d3137f989bf88f092d17198a20f794a5
ts437 <tim.scholtes@datasciencenetwork.com>
2017-02-02 22:42:37 +0000

    made some decent progress towards having proper rules in place for me to train

--- main.py
+++ main.py
@@ -1,41 +1,87 @@
 
 def parse_command(instr, bot, pos):
-    if instr.startswith('action move'):
-        time = int(instr.split(' ')[-1])
-        x, y = bot.get_move(pos, time)
-        return 'place_move %d %d\n' % (x, y)
-    elif instr.startswith('update game field'):
-        fstr = instr.split(' ')[-1]
-        pos.parse_field(fstr)
-    elif instr.startswith('update game macroboard'):
-        mbstr = instr.split(' ')[-1]
-        pos.parse_macroboard(mbstr)
-    elif instr.startswith('update game move'):
-        pos.nmove = int(instr.split(' ')[-1])
-    elif instr.startswith('settings your_botid'):
-        myid = int(instr.split(' ')[-1])
-        bot.myid = myid
-        bot.oppid = 1 if myid == 2 else 2
-    elif instr.startswith('settings timebank'): 
-        bot.timebank = int(instr.split(' ')[-1])
-    elif instr.startswith('settings time_per_move'): 
-        bot.time_per_move = int(instr.split(' ')[-1])
-    return ''
+	if instr.startswith('action move'):
+		time = int(instr.split(' ')[-1])
+		x, y = bot.get_move(pos, time)
+		pos.make_move()
+		return 
+		#return 'place_move %d %d\n' % (x, y)
+	elif instr.startswith('update game field'):
+		fstr = instr.split(' ')[-1]
+		pos.parse_field(fstr)
+	elif instr.startswith('update game macroboard'):
+		mbstr = instr.split(' ')[-1]
+		pos.parse_macroboard(mbstr)
+	elif instr.startswith('update game move'):
+		pos.nmove = int(instr.split(' ')[-1])
+	elif instr.startswith('settings your_botid'):
+		myid = int(instr.split(' ')[-1])
+		bot.myid = myid
+		bot.oppid = 1 if myid == 2 else 2
+	elif instr.startswith('settings timebank'): 
+		bot.timebank = int(instr.split(' ')[-1])
+		print 'updating timebank'
+	elif instr.startswith('settings time_per_move'): 
+		bot.time_per_move = int(instr.split(' ')[-1])
+	return ''
+
+
+def play_game(pos,max_counter,*players):
+	pid = 1
+	tleft=1000
+	counter = 0
+	while counter < max_counter:
+		counter += 1
+		print 'counter:',counter
+		for player in players:
+			print pid
+			print pos.legal_moves()
+			pos.get_board()
+			pos.get_macroboard()
+			pos.get_win_macroboard()
+			x,y = player.get_move(pos,tleft)
+			print 'player:',pid,'makes move:',x,y
+			pos.make_move(x,y,pid)
+
+			# determine allowable next move:
+			mbx = x / 3
+			mby = y / 3
+			j = mby * 3 + mbx
+			if pos.win_macroboard[j] == -1:
+				pos.macroboard = [-1 if i == j else 0 for i in range(9)]
+			else:
+				pos.macroboard = [-1 for i in range(9)]
+			pos.determine_win_macroboard(x,y,pid)
+
+			# check terminal state
+			term = pos.terminal_state(pid)
+			if term:
+				return pid
+
+			pid = 3 - pid
+
+
+
+
 
 if __name__ == '__main__':
-    import sys
-    from position import Position
-    from randombot import RandomBot
+	import sys
+	from position import Position
+	from randombot import RandomBot
 
-    pos = Position()
-    bot = RandomBot()
-    
-    while True:
-        try:
-            instr = raw_input()
-        except Exception as e:
-            sys.stderr.write('error reading input')
-        outstr = parse_command(instr, bot, pos)
-        sys.stdout.write(outstr)
-        sys.stdout.flush()            
-            
+
+	pos = Position()
+	bot1 = RandomBot()
+	bot2 = RandomBot()
+	
+	outcome = play_game(pos,200,bot1,bot2)
+	print 'winner is:',outcome
+
+
+	
+
+
+
+
+
+
--- position.py
+++ position.py
@@ -1,33 +1,72 @@
+import itertools
 
 class Position:
-    
-    def __init__(self):
-        self.board = []
-        self.macroboard = []
-    
-    def parse_field(self, fstr):
-        flist = fstr.replace(';', ',').split(',')
-        self.board = [ int(f) for f in flist ]
-    
-    def parse_macroboard(self, mbstr):
-        mblist = mbstr.replace(';', ',').split(',')
-        self.macroboard = [ int(f) for f in mblist ]
-    
-    def is_legal(self, x, y):
-        mbx, mby = x/3, y/3
-        return self.macroboard[3*mby+mbx] == -1 and self.board[9*y+x] == 0
+	
+	def __init__(self):
+		self.board = [0 for i in range(81)]
+		self.macroboard = [-1 for i in range(9)]
+		self.win_macroboard = [-1 for i in range(9)]
+		self.win_combos = [range(3),range(2,5),range(5,8),
+		[0,3,6],[1,4,7],[2,5,8],[0,4,8],[2,4,6]]
+	
+	def parse_field(self, fstr):
+		flist = fstr.replace(';', ',').split(',')
+		self.board = [ int(f) for f in flist ]
+	
+	def parse_macroboard(self, mbstr):
+		mblist = mbstr.replace(';', ',').split(',')
+		self.macroboard = [ int(f) for f in mblist ]
+	
+	def is_legal(self, x, y):
+		mbx, mby = x/3, y/3
+		return self.macroboard[3*mby+mbx] == -1 and self.board[9*y+x] == 0
 
-    def legal_moves(self):
-        return [ (x, y) for x in range(9) for y in range(9) if self.is_legal(x, y) ]
-        
-    def make_move(self, x, y, pid):
-        mbx, mby = x/3, y/3
-        self.macroboard[3*mby+mbx] = -1
-        self.board[9*y+x] = pid
-        
-    def get_board(self):
-        return ''.join(self.board, ',')
+	def legal_moves(self):
+		return [ (x, y) for x in range(9) for y in range(9) if self.is_legal(x, y) ]
+		
+	def make_move(self, x, y, pid):
+		mbx, mby = x/3, y/3
+		self.macroboard[3*mby+mbx] = -1
+		self.board[9*y+x] = pid
+		
+	def get_board(self):
+		line = []
+		X = self.board
+		for i in range(9):
+			for x in X[i*9:(i+1)*9]:
+				line.append(' {} '.format(x))
+			print ''.join(line)
+			line=[]
+		pass
 
-    def get_macroboard(self):
-        return ''.join(self.macroboard, ',')
-    
+	def get_macroboard(self):
+		for row in range(3):
+			print self.macroboard[row*3:(row+1)*3]
+		pass
+
+	def get_win_macroboard(self):
+		for row in range(3):
+			print self.win_macroboard[row*3:(row+1)*3]
+		pass
+	
+	def determine_win_macroboard(self,x,y,pid):
+		mbx, mby = x/3, y/3
+		mini_board = [self.board[mby*27+mbx*3+0:mby*27+mbx*3+3],
+			self.board[mby*27+mbx*3+9:mby*27+mbx*3+12],
+				self.board[mby*27+mbx*3+18:mby*27+mbx*3+21]]
+		mini_board = list(itertools.chain(*mini_board))
+		print 'mini'
+		print mini_board
+		for combo in self.win_combos:
+			if all(mini_board[x] == pid for x in combo):
+				self.win_macroboard[3*mby+mbx] = pid
+				pass
+		if all(mini_board[x] != 0 for x in range(9)):
+			self.win_macroboard[3*mby+mbx] = 0
+			pass
+
+	def terminal_state(self,pid):
+		for combos in self.win_combos:
+			if all(self.win_macroboard[x] == pid for x in combos):
+				return True
+
